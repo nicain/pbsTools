a2d1818852ad2d0c3dcffa11c19d56dfeeaa67f2
Nicholas Cain <nicain.seattle@gmail.com>
2010-04-30 00:45:05 -0700

    One more typo, I swear

--- pbsTools.py
+++ pbsTools.py
@@ -123,7 +123,7 @@ def runPBS(commandString, fileList = (), wallTime = 30*60, nodes = 'default', pp
 				
 		# Either local or not, if we did a wallTimeEst, display results:
 		if settings['server'] == 'wallTimeEstimate':
-			for file in getFileIterator(settings['outputDir'], wallTimeEstData.dat):
+			for file in getFileIterator(settings['outputDir'], 'wallTimeEstData.dat'):
 				call('more ' + file)
 
 	return settings
