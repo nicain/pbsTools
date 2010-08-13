1134c34ece482218681c6ae968c66a971863c560
Nicholas Cain <nicain.seattle@gmail.com>
2010-08-13 19:03:45 +0000

    Checking about server avail

--- pbsTools.py
+++ pbsTools.py
@@ -263,7 +263,7 @@ def runPBS(
 			jobs=[0]*len(jobList)
 			for input in jobList:
 				jobs[counter] = job_server.submit(doTheMagic,(input[0],input[1],niceLevel), (), ("subprocess","os"))
-				print '  Job '+str(counter+1)+' started... (nice = ' + str(niceLevel) + ')' 
+				print '    Job '+str(counter+1)+' started... (nice = ' + str(niceLevel) + ')' 
 				counter += 1
 		
 			# Wait for the jobs:  (Note: this is forced for now... might change later... )
