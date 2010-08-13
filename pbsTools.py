e6d3604ee4c963dbbba4dee1b011a2068fafbeab
Nicholas Cain <nicain.seattle@gmail.com>
2010-08-13 01:50:44 +0000

    Added Niceness

--- pbsTools.py
+++ pbsTools.py
@@ -22,7 +22,8 @@ def runPBS(
 	nodes = 'default', 
 	ppn = 'default', 
 	repspp = 'default',			# Probably never change
-	verbose = 1):
+	verbose = 1,
+	niceNess = 10):
 
 	##### Option for runLocation ######
 	# local (default)
@@ -70,6 +71,7 @@ def runPBS(
 	settings['hiddenDir'] = os.path.abspath(os.path.expanduser(hiddenDir))
 	settings['outputDir'] = os.path.abspath(os.path.expanduser(outputDir))
 	settings['verbose'] = verbose
+	settings['niceNess'] = niceNess
 
 	settings['cwd'] = os.getcwd()
 	settings['qSubCommand'] = 'qsub -S /bin/tcsh -q '
@@ -201,7 +203,8 @@ def runPBS(
 			
 			# This is the function that runs each job, over the shared file system:
 			def doTheMagic(where, fileName):
-				subprocess.call(os.path.join(where,fileName),shell=True,cwd=where)
+				prefix = 'nice -n ' + str(settings['niceNess'])
+				subprocess.call(prefix+' '+os.path.join(where,fileName),shell=True,cwd=where)
 				return 0
 			
 			# Gather names and directories of all jobs:
