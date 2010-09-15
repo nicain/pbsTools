322b4be9cb201882cd349d719d7f9faf4f6e164a
Nicholas Cain <nicain.seattle@gmail.com>
2010-09-15 03:19:45 +0000

    new CPU load file

---
+++ getCPULoad.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# getCPULoad.sh
+# pbsTools
+#
+# Created by nicain on 9/8/10.
+# Copyright 2010 University of Washington. All rights reserved.
+
+totalUsage=`ps aux|awk 'NR > 0 { s +=$3 }; END {print s}'`
+nCPUs=`cat /proc/cpuinfo | grep processor | wc -l`
+echo $(($totalUsage/$nCPUs))
